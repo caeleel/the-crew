706cfd98ff0b95b567a88102f5b137f930a3fd12
Karl Jiang <fingingthenumber@gmail.com>
2024-08-05 17:31:44 -0700

    number feedback

--- src/app/cards.tsx
+++ src/app/cards.tsx
@@ -27,6 +27,14 @@ const suitToImg = {
   s: 'black.png',
 }
 
+const suitToOutline = {
+  Y: '#EAB308',
+  P: '#F87171',
+  G: '#22C55E',
+  B: '#0EA5E9',
+  s: 'black',
+}
+
 export function Signal({ hint }: { hint: Hint | null }) {
   if (!hint) {
     return <div className="w-4 h-4 border-green-400 border rounded-full" />
@@ -80,7 +88,7 @@ export function Card({
 }) {
   const suit = card[0] as SuitWithSubs
   const cardSize = big ? 'w-12 h-16' : 'w-3 h-5'
-  const numberSize = big ? 'text-lg' : ''
+  const numberSize = big ? 'text-4xl' : ''
   const halfDims = 'w-1.5 h-2.5'
 
   return (
@@ -111,7 +119,14 @@ export function Card({
         <div
           className={`absolute ${cardSize} flex items-center justify-center top-0 left-0`}
         >
-          <div className={`${numberSize} drop-shadow`}>{card[1]}</div>
+          <div
+            className={`${numberSize} drop-shadow`}
+            style={{
+              WebkitTextStroke: big ? `1px ${suitToOutline[suit]}` : '',
+            }}
+          >
+            {card[1]}
+          </div>
         </div>
       )}
       {multiplier && (
